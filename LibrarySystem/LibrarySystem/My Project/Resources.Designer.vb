edc4058b7b2edc27398cde698c1ceaf401b30ba7
anotchung123 <48198264+ahweichung123@users.noreply.github.com>
2020-04-27 21:52:17 +0800

    andy commit (27 Apr, 2152)

--- LibrarySystem/LibrarySystem/My Project/Resources.Designer.vb
+++ LibrarySystem/LibrarySystem/My Project/Resources.Designer.vb
@@ -113,6 +113,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Book_Return_sc() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Book_Return_sc", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property books_glasses_table_library() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("books_glasses_table_library", resourceCulture)
@@ -143,6 +153,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property new_book_sc() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("new_book_sc", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property pngguru_com__1_() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("pngguru_com__1_", resourceCulture)
@@ -157,6 +177,26 @@ Namespace My.Resources
             Get
                 Dim obj As Object = ResourceManager.GetObject("pngguru_com__5_", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property Search_book_sc() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Search_book_sc", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        '''</summary>
+        Friend ReadOnly Property system_icon() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("system_icon", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
             End Get
         End Property
     End Module
--- LibrarySystem/LibrarySystem/Others/HowToUse.Designer.vb
+++ LibrarySystem/LibrarySystem/Others/HowToUse.Designer.vb
@@ -44,26 +44,29 @@ Partial Class HowToUse
         Me.panelHome.AutoScroll = True
         Me.panelHome.Controls.Add(Me.lblHomeDetails)
         Me.panelHome.Controls.Add(Me.PictureBox2)
-        Me.panelHome.Location = New System.Drawing.Point(49, 184)
+        Me.panelHome.Location = New System.Drawing.Point(37, 150)
+        Me.panelHome.Margin = New System.Windows.Forms.Padding(2)
         Me.panelHome.Name = "panelHome"
-        Me.panelHome.Size = New System.Drawing.Size(919, 537)
+        Me.panelHome.Size = New System.Drawing.Size(689, 425)
         Me.panelHome.TabIndex = 0
         '
         'lblHomeDetails
         '
         Me.lblHomeDetails.Font = New System.Drawing.Font("HoloLens MDL2 Assets", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblHomeDetails.Location = New System.Drawing.Point(33, 394)
+        Me.lblHomeDetails.Location = New System.Drawing.Point(25, 320)
+        Me.lblHomeDetails.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblHomeDetails.Name = "lblHomeDetails"
-        Me.lblHomeDetails.Size = New System.Drawing.Size(791, 751)
+        Me.lblHomeDetails.Size = New System.Drawing.Size(593, 610)
         Me.lblHomeDetails.TabIndex = 6
         Me.lblHomeDetails.Text = "Home details"
         '
         'PictureBox2
         '
         Me.PictureBox2.Image = Global.LibrarySystem.My.Resources.Resources.home
-        Me.PictureBox2.Location = New System.Drawing.Point(96, 13)
+        Me.PictureBox2.Location = New System.Drawing.Point(72, 11)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(728, 356)
+        Me.PictureBox2.Size = New System.Drawing.Size(546, 289)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 0
         Me.PictureBox2.TabStop = False
@@ -74,44 +77,49 @@ Partial Class HowToUse
         Me.panelBorrow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.panelBorrow.Controls.Add(Me.lblBorrowDetails)
         Me.panelBorrow.Controls.Add(Me.PictureBox3)
-        Me.panelBorrow.Location = New System.Drawing.Point(49, 184)
+        Me.panelBorrow.Location = New System.Drawing.Point(37, 150)
+        Me.panelBorrow.Margin = New System.Windows.Forms.Padding(2)
         Me.panelBorrow.Name = "panelBorrow"
-        Me.panelBorrow.Size = New System.Drawing.Size(919, 534)
+        Me.panelBorrow.Size = New System.Drawing.Size(689, 425)
         Me.panelBorrow.TabIndex = 11
         '
         'lblBorrowDetails
         '
         Me.lblBorrowDetails.Font = New System.Drawing.Font("HoloLens MDL2 Assets", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblBorrowDetails.Location = New System.Drawing.Point(23, 360)
+        Me.lblBorrowDetails.Location = New System.Drawing.Point(17, 292)
+        Me.lblBorrowDetails.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblBorrowDetails.Name = "lblBorrowDetails"
-        Me.lblBorrowDetails.Size = New System.Drawing.Size(864, 520)
+        Me.lblBorrowDetails.Size = New System.Drawing.Size(648, 422)
         Me.lblBorrowDetails.TabIndex = 7
         Me.lblBorrowDetails.Text = "Borrow details"
         '
         'PictureBox3
         '
         Me.PictureBox3.Image = Global.LibrarySystem.My.Resources.Resources.borrow
-        Me.PictureBox3.Location = New System.Drawing.Point(230, -1)
+        Me.PictureBox3.Location = New System.Drawing.Point(153, 10)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(537, 345)
+        Me.PictureBox3.Size = New System.Drawing.Size(403, 280)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox3.TabIndex = 0
         Me.PictureBox3.TabStop = False
         '
         'btnNext
         '
-        Me.btnNext.Location = New System.Drawing.Point(868, 137)
+        Me.btnNext.Location = New System.Drawing.Point(651, 111)
+        Me.btnNext.Margin = New System.Windows.Forms.Padding(2)
         Me.btnNext.Name = "btnNext"
-        Me.btnNext.Size = New System.Drawing.Size(100, 30)
+        Me.btnNext.Size = New System.Drawing.Size(75, 24)
         Me.btnNext.TabIndex = 9
         Me.btnNext.Text = "&Next"
         Me.btnNext.UseVisualStyleBackColor = True
         '
         'btnPrevious
         '
-        Me.btnPrevious.Location = New System.Drawing.Point(749, 137)
+        Me.btnPrevious.Location = New System.Drawing.Point(562, 111)
+        Me.btnPrevious.Margin = New System.Windows.Forms.Padding(2)
         Me.btnPrevious.Name = "btnPrevious"
-        Me.btnPrevious.Size = New System.Drawing.Size(100, 30)
+        Me.btnPrevious.Size = New System.Drawing.Size(75, 24)
         Me.btnPrevious.TabIndex = 10
         Me.btnPrevious.Text = "&Previous"
         Me.btnPrevious.UseVisualStyleBackColor = True
@@ -119,11 +127,12 @@ Partial Class HowToUse
         'Label1
         '
         Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Hobo Std", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(184, 63)
+        Me.Label1.Location = New System.Drawing.Point(138, 51)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(412, 69)
+        Me.Label1.Size = New System.Drawing.Size(309, 56)
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "How to use"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -132,24 +141,27 @@ Partial Class HowToUse
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox1.Image = Global.LibrarySystem.My.Resources.Resources.pngguru_com__5_
-        Me.PictureBox1.Location = New System.Drawing.Point(49, 35)
+        Me.PictureBox1.Location = New System.Drawing.Point(37, 28)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(92, 116)
+        Me.PictureBox1.Size = New System.Drawing.Size(69, 94)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 7
         Me.PictureBox1.TabStop = False
         '
         'HowToUse
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1006, 721)
+        Me.ClientSize = New System.Drawing.Size(754, 586)
         Me.Controls.Add(Me.panelBorrow)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.btnNext)
         Me.Controls.Add(Me.panelHome)
         Me.Controls.Add(Me.btnPrevious)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "HowToUse"
         Me.Text = "How To Use"
         Me.panelHome.ResumeLayout(False)
